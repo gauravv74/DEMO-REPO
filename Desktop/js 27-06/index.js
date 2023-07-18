56c398f5e54631582c7081e2c7b855ed9e1f99bb
gauravv74 <gauravmgangurde@gmail.com>
2023-07-18 22:27:25 +0530

    demo commit

---
+++ Desktop/js 27-06/index.js
@@ -0,0 +1,39 @@
+//function Expression
+// const isOdd = (...numbers)=>{
+//     let total =0;
+//     for(let number  of numbers){
+//         total = total + number;
+//     }
+//     return total;
+// }
+// let ans =isOdd(1,2,3,4,5,6,7,8,9,10);
+// console.log(ans);
+
+
+// const  details = {
+//     name :"Gaurav",
+//     age : 21,
+//     gander : "male"
+// }
+//  const arr=[10,20,30,40,50];
+
+//  const detailsFunction = (obj)=>{
+//     for(let number of obj ){
+//         console.log(number)
+//     }
+//  }
+
+//  detailsFunction(arr);
+
+
+
+let x =1;
+
+{
+   var a = 100;
+    let x = 20;
+    console.log(x);
+  console.log(a);
+
+ }
+  console.log(x);
